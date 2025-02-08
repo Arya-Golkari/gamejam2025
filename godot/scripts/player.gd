51a6afac8c7cee660d7800d2a525daa35104cb04
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-02-08 15:39:00 +0000

    Added gravity and jumping

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -1,19 +1,23 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 400
 
-func get_input():
-	var direction = Input.get_vector("Left", "Right", "None", "None")
-	velocity = speed * direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 	
 func _physics_process(delta: float) -> void:
-	get_input()
-	move_and_slide()
+	var direction = Input.get_axis("Left", "Right")
+	velocity.x = speed * direction
 
+	velocity += delta * get_gravity() * 10
+
+	if Input.is_action_pressed("Jump"):
+		print("test")
+		velocity.y = -1000
+
+	move_and_slide()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
