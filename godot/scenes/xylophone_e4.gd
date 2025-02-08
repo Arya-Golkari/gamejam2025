285aa8e545d19c52cf33f0289e2eac68a35f32ba
Arya Golkari <ag2451@cam.ac.uk>
2025-02-08 14:38:41 +0000

    Added xylophone.

---
+++ godot/scenes/xylophone_e4.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var audio_player = $XylophoneE4Player
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			audio_player.play()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
