646cd30ee3eb8c0d8f521a7626de718a8bf8f03c
Arya Golkari <ag2451@cam.ac.uk>
2025-02-08 20:20:51 +0000

    Level 1 complete

--- godot/scripts/level_1.gd
+++ godot/scripts/level_1.gd
@@ -4,6 +4,9 @@ var notes = Array()
 @export var fanfare_player_path : NodePath
 var fanfare_player : AudioStreamPlayer
 
+var done : bool = false
+var fanfare_done : bool = false
+
 @export var closed_door : Texture2D
 @export var open_door: Texture2D
 
@@ -20,37 +23,42 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(notes)
-	if notes.slice(-6, notes.size()+1) == ["E4", "C4", "C4", "E4", "E4", "C4"]:
+	if (notes.slice(-6, notes.size()+1) == ["E4", "C4", "C4", "E4", "E4", "C4"]) and (not fanfare_done):
+		done = true
+		
+	if done and (not fanfare_done):
+		fanfare_done = true
 		timer.start(1)
 		fanfare_player.play()
 		
 		await timer.timeout
 		background.texture = open_door
+		
+		done = false
+
+
+func fanfare() -> void:
+	timer.start(1)
+	fanfare_player.play()
+	
+	await timer.timeout
+	background.texture = open_door
+
+
+func play_random() -> void:
+	var random_int = randi_range(0, 1)
+	if random_int == 0:
+		c4_player.play()
+	else:
+		e4_player.play()
 
 
 func background_music() -> void:
-	timer.start(0.5)
-	c4_player.play()
-	
-	await timer.timeout
-	
-	timer.start(0.5)
-	e4_player.play()
-	
-	await timer.timeout
-	
-	timer.start(0.5)
-	c4_player.play()
-	
-	await timer.timeout
-	
-	timer.start(0.5)
-	e4_player.play()
-	
-	await timer.timeout
-	
-	c4_player.play()
+	for i in range(5):
+		timer.start(0.5)
+		play_random()
+		
+		await timer.timeout
 
 
 func _on_xylophone_note(note: String) -> void:
--- godot/scripts/play_background.gd
+++ godot/scripts/play_background.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 signal play_background
+var original_scale = Vector2(1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	scale = 2 * original_scale
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +18,13 @@ func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			emit_signal("play_background")
+
+
+#func _on_mouse_entered() -> void:
+	#print("mouse entered")
+	#scale = original_scale * 2
+#
+#
+#func _on_mouse_exited() -> void:
+	#print("mouse exited")
+	#scale = original_scale
