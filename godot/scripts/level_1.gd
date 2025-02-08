e3e821f2a3cddba4bce8b4ede813d180a521a1aa
Arya Golkari <ag2451@cam.ac.uk>
2025-02-08 17:17:16 +0000

    Level 1

--- godot/scripts/level_1.gd
+++ godot/scripts/level_1.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
 var notes = Array()
-@export var audio_player_path : NodePath
-var audio_player : AudioStreamPlayer
+@export var fanfare_player_path : NodePath
+var fanfare_player : AudioStreamPlayer
+@onready var c4_player : AudioStreamPlayer = $XylophoneC4Player
+@onready var e4_player : AudioStreamPlayer = $XylophoneE4Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	audio_player = get_node(audio_player_path)
+	fanfare_player = get_node(fanfare_player_path)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(notes.slice(-7, notes.size()))
 	if notes.slice(-7, notes.size()) == ["C4", "C4", "D4", "D4", "E4", "E4", "D4"]:
-		audio_player.play()
+		fanfare_player.play()
+
+
+func background_music() -> void:
+	pass
 
 
 func _on_xylophone_note(note: String) -> void:
