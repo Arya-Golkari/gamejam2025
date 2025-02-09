b7d468678e24a855d1617c9e1fc2c929cfd44445
Arya Golkari <ag2451@cam.ac.uk>
2025-02-09 13:50:12 +0000

    Player kinematics

--- godot/scripts/level_1.gd
+++ godot/scripts/level_1.gd
@@ -14,11 +14,26 @@ var fanfare_done : bool = false
 @onready var e4_player : AudioStreamPlayer = $XylophoneE4Player
 @onready var timer : Timer = $Timer
 @onready var background : Sprite2D = $Background
+@onready var player : Node2D = $Player
+@onready var wall_left : Node2D = $WallLeft
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	background.texture = closed_door
 	fanfare_player = get_node(fanfare_player_path)
+	
+	var position_x = -245
+	player.position = Vector2(position_x, 530)
+	wall_left.position = Vector2(position_x + 40, 390)
+	
+	timer.start(1)
+	await timer.timeout
+	
+	while position_x < -30:
+		position_x += 2
+		await get_tree().create_timer(0.01).timeout
+		player.position = Vector2(position_x, player.position.y)
+		wall_left.position = Vector2(position_x + 40, wall_left.position.y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -1,20 +1,28 @@
 extends CharacterBody2D
 
 @export var speed = 800
+@export var walking : Texture2D
+@export var jumping : Texture2D
+
+@onready var sprite : Sprite2D = $Sprite
 
 #testing
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	sprite.texture = walking
 	
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("Left", "Right")
 	velocity.x = speed * direction
 
 	velocity += delta * get_gravity() * 10
+	
+	if is_on_floor():
+		sprite.texture = walking
 
 	if is_on_floor() and Input.is_action_pressed("Jump"):
 		velocity.y = -2000
+		sprite.texture = jumping
 
 	move_and_slide()
 	
@@ -23,11 +31,10 @@ func _physics_process(delta: float) -> void:
 		var nextScene
 		if curScene == "level0.tscn":
 			nextScene = "res://scenes/level1.tscn"
-		elif curScene == "level1.tscn":
-			nextScene = "res://scenes/level2.tscn"
 			
 		get_tree().change_scene_to_file(nextScene)
-			
+
+>>>>>>> f7125b5 (Player kinematics)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
