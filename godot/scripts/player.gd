a1ee606648e0c7a1baf5085c2412f63751657d8d
awrreny <awrreny@gmail.com>
2025-02-09 13:43:14 +0000

    added logic to change level when the player moves to the right edge

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 800
 
-
+#testing
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,6 +17,17 @@ func _physics_process(delta: float) -> void:
 		velocity.y = -2000
 
 	move_and_slide()
+	
+	if global_position.x > 1900:
+		var curScene = get_tree().current_scene.scene_file_path.split("/")[-1]
+		var nextScene
+		if curScene == "level0.tscn":
+			nextScene = "res://scenes/level1.tscn"
+		elif curScene == "level1.tscn":
+			nextScene = "res://scenes/level2.tscn"
+			
+		get_tree().change_scene_to_file(nextScene)
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
